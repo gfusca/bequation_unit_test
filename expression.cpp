043c71fb83cbe2a2bee5fd8e8e949ec68c521f36
Gabriel Fusca <gabrielfusca@gmail.com>
2015-04-09 11:17:37 -0300

    Se agrega operador booleano relacionado con la ecuación

--- expression.cpp
+++ expression.cpp
@@ -1,15 +1,28 @@
 #include <string>
 #include "expression.h"
+#include "operator.h"
+
+BooleanExpression::BooleanExpression() : operator_(NULL) {
+}
+
+BooleanExpression::~BooleanExpression() {
+	delete operator_;
+}
 
 void BooleanExpression::substitute(const std::string& ArgumentIdentifier, bool Value) {
+	operator_->substitute(ArgumentIdentifier, Value);
 }
 
 bool BooleanExpression::evaluate() {
-	return false;
+	return operator_->evaluate();
 }
 
 BooleanExpression* BooleanExpression::Create(const std::string& Expression) {
+	throw ExpressionParseException("Error parsing expression " + Expression);
 	return NULL;
 }
 	
+std::string BooleanExpression::getExpressionAsString() const {
+	return operator_->getExpressionAsString();
+}
 
--- expression.h
+++ expression.h
@@ -1,12 +1,17 @@
 #ifndef __EXPRESSION_H__
 #define __EXPRESSION_H__
-
+#include <ostream>
 #include <string>
+#include <exception>
+
+class BooleanOperator;
 
 class BooleanExpression {
 	BooleanExpression(const std::string& BooleanExpression);
 
 public:
+	BooleanExpression();
+	~BooleanExpression();
 	void substitute(const std::string& ArgumentIdentifier, bool Value);
 	bool evaluate();
 	std::string getExpressionAsString()const;
@@ -15,6 +20,22 @@ public:
 		return stream;
 	}
 	static BooleanExpression* Create(const std::string& Expression);
+
+private:
+	BooleanOperator* operator_;
 };
 
+class ExpressionParseException : std::exception {
+
+public:
+	ExpressionParseException(const std::string& Message) : msg_(Message) {
+	}
+	virtual ~ExpressionParseException() throw() {}
+	std::string getMessage() {
+		return msg_;
+	}
+
+protected:
+	std::string msg_;
+};
 #endif	// __EXPRESSION_H__
