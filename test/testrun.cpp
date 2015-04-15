9fc2866adf64c368e6d4e3b0059e93de59e3af8e
Gabriel Fusca <gabrielfusca@gmail.com>
2015-04-15 18:54:47 -0300

    se pone método parse como instancia

--- test/testrun.cpp
+++ test/testrun.cpp
@@ -1,7 +1,23 @@
 // #include "expressiontest.h"
 #include <gtest/gtest.h>
+#include <string>
+#include <iostream>
+#include "mockexpression.h"
+
+using testing::ReturnNew;
+using testing::Return;
+
+TEST(MockTestCase, MockTestString) {
+	std::string eq = "AND(OR(a,b),AND(c,d))";
+	MockOperator *op = new MockOperator;
+	MockExpression expression(op);
+	EXPECT_CALL(expression, getExpressionAsString()).WillOnce(Return(eq));
+	EXPECT_STREQ(eq.c_str(), expression.getExpressionAsString().c_str());
+}
+
 
 int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
+  	testing::InitGoogleMock(&argc, argv);
   	return RUN_ALL_TESTS();	
 }
